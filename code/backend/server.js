be4f32285008746996c7b1a6006a1d0ed158cc65
jacobgrieninger <72899444+jacobgrieninger@users.noreply.github.com>
2022-11-06 14:47:19 -0500

    prepping deployment

--- code/backend/server.js
+++ code/backend/server.js
@@ -1,6 +1,7 @@
-const express = require("express");
-const connectDB = require("./config/db");
+const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
+const path = require('path');
 
 //Connect Db
 connectDB();
@@ -9,28 +10,35 @@ connectDB();
 app.use(express.json({ extended: false }));
 
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
   );
   res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type'
   );
-  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
 
-app.get("/", (req, res) => res.send("API Running"));
-
 //Define Routes
-app.use("/api/users", require("./routes/api/users"));
-app.use("/api/auth", require("./routes/api/auth"));
-app.use("/api/schedule", require("./routes/api/schedule"));
-app.use("/api/staticSchedule", require("./routes/api/staticSchedule"));
-app.use("/api/availability", require("./routes/api/availability"));
-app.use("/api/timeoff", require("./routes/api/timeoff"));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/schedule', require('./routes/api/schedule'));
+app.use('/api/staticSchedule', require('./routes/api/staticSchedule'));
+app.use('/api/availability', require('./routes/api/availability'));
+app.use('/api/timeoff', require('./routes/api/timeoff'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('../production/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, '../production', 'build', 'index.html')
+    );
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
